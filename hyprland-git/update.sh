60fcb4fd89ccdb89df9312828bcffd86f407d416
Pavel Solovev <daron439@gmail.com>
2023-05-27 15:17:07 +0200

    fixup

--- hyprland-git/update.sh
+++ hyprland-git/update.sh
@@ -29,5 +29,5 @@ sed -i "/^Version:/s/$oldTag/$newTag/" hyprland-git.spec
 
 git diff --quiet || \
 perl -pe 's/(?<=bumpver\s)(\d+)/$1 + 1/ge' -i hyprland-git.spec && \
-git commit -am "up rev hyprland-git-${newTag}+${newHyprlandCommit:0:7}" # && \
-#git push
+git commit -am "up rev hyprland-git-${newTag}+${newHyprlandCommit:0:7}" && \
+git push
