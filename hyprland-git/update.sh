5a2965584337f1f445bc337fa581cfb9d99c57df
Pavel Solovev <daron439@gmail.com>
2023-05-27 15:03:32 +0200

    add action

--- hyprland-git/update.sh
+++ hyprland-git/update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 set -eux
 
-modified=0
+oldTag="$(rpmspec -q --qf "%{version}\n" hyprland-git.spec | head -1 | sed 's/\^.*//')"
+newTag="$(curl "https://api.github.com/repos/hyprwm/Hyprland/tags" | jq -r '.[0].name' | sed 's/^v//')"
 
 oldHyprlandCommit="$(sed -n 's/.*hyprland_commit \(.*\)/\1/p' hyprland-git.spec)"
 newHyprlandCommit="$(curl -s -H "Accept: application/vnd.github.VERSION.sha" "https://api.github.com/repos/hyprwm/Hyprland/commits/main")"
@@ -12,19 +13,21 @@ newWlrootsCommit="$(curl -L \
             -H "X-GitHub-Api-Version: 2022-11-28" \
             https://api.github.com/repos/hyprwm/Hyprland/contents/subprojects/wlroots | jq -r '.sha')"
 
-if [[ $oldHyprlandCommit != "$newHyprlandCommit" ]]; then
-    modified+=1
-    sed -i "s/$oldHyprlandCommit/$newHyprlandCommit/" hyprland-git.spec
-fi
+oldProtocolsCommit="$(sed -n 's/.*protocols_commit \(.*\)/\1/p' hyprland-git.spec)"
+newProtocolsCommit="$(curl -L \
+            -H "Accept: application/vnd.github+json" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            https://api.github.com/repos/hyprwm/Hyprland/contents/subprojects/hyprland-protocols | jq -r '.sha')"
 
-if [[ $oldWlrootsCommit != "$newWlrootsCommit" ]]; then
-    modified+=1
-    sed -i "s/$oldWlrootsCommit/$newWlrootsCommit/" hyprland-git.spec
-fi
+sed -i "s/$oldHyprlandCommit/$newHyprlandCommit/" hyprland-git.spec
 
-if [[ $modified -ge 1 ]]; then
-    perl -pe 's/(?<=bumpver\s)(\d+)/$1 + 1/ge' -i hyprland-git.spec
-fi
+sed -i "s/$oldWlrootsCommit/$newWlrootsCommit/" hyprland-git.spec
 
-git --no-pager diff
-git commit -am "up rev"
+sed -i "s/$oldProtocolsCommit/$newProtocolsCommit/" hyprland-git.spec
+
+sed -i "/^Version:/s/$oldTag/$newTag/" hyprland-git.spec
+
+git diff --quiet || \
+perl -pe 's/(?<=bumpver\s)(\d+)/$1 + 1/ge' -i hyprland-git.spec && \
+git commit -am "up rev hyprland-git-${newTag}+${newHyprlandCommit:0:7}" # && \
+#git push
