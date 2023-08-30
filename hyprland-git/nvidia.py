71233263a322902f66163d3635725c96a0a22f27
solopasha <pasha@solopasha.ru>
2023-08-30 21:02:03 +0300

    fixup

--- hyprland-git/nvidia.py
+++ hyprland-git/nvidia.py
@@ -4,6 +4,6 @@ from specfile import Specfile
 specfile = Specfile('hyprland-git.spec')
 
 specfile.add_patch('nvidia.patch')
-specfile.name = os.getenv('SPECFILE').removesuffix('.spec')
+specfile.name = os.getenv('specfile').removesuffix('.spec')
 
 print(specfile)
