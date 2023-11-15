372ec4526f5294ed136d6277f060d76da3e1b013
Pavel Solovev <daron439@gmail.com>
2023-11-16 00:26:33 +0300

    branch out on release

--- hyprland-git/update.sh
+++ hyprland-git/update.sh
@@ -55,7 +55,13 @@ copr-cli build-package solopasha/hyprland --nowait --name hyprland-plugins-git -
 copr-cli build-package solopasha/hyprland --nowait --name hyprland-nvidia-git; }
 
 if [[ $newRelease == "1" ]]; then
+    parallel copr edit-package-scm --name {} --clone-url "https://github.com/solopasha/hyprlandRPM.git" --commit master \
+        --subdir hyprland-git --spec {}.spec --type git --method make_srpm --max-builds 5 solopasha/hyprland ::: hyprland{,-nvidia,-legacyrenderer}
     hyprlandBuildId=$(copr-cli build-package solopasha/hyprland --nowait --name hyprland | sed -n 's/.*builds: \(.*\)/\1'/p)
     parallel copr-cli build-package solopasha/hyprland --nowait --name ::: hyprland{-nvidia,-legacyrenderer}
     copr-cli build-package solopasha/hyprland --nowait --name hyprland-plugins --after-build-id "$hyprlandBuildId"
+    git branch "$newTag"
+    git push origin "$newTag"
+    parallel copr edit-package-scm --name {} --clone-url "https://github.com/solopasha/hyprlandRPM.git" --commit "$newTag" \
+        --subdir hyprland-git --spec {}.spec --type git --method make_srpm --max-builds 5 solopasha/hyprland ::: hyprland{,-nvidia,-legacyrenderer}
 fi
